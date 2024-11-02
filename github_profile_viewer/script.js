d4adaa0ebd255549a8e547e71da52953c0c2a31e
Kherzie Andal <kherzieandal@gmail.com>
2024-11-02 09:15:48 +0800

    Added catch & error handling/prompt

--- github_profile_viewer/script.js
+++ github_profile_viewer/script.js
@@ -1,15 +1,20 @@
 const fetchGithub = () => {
     const username = document.getElementById('usernameInput').value
-    console.log(`The username is: ${username}`);
 
     fetch(`https://api.github.com/users/${username}`)
         .then((res) => res.json())
         .then((data) => {
             showGithubDetails(data);
         })
+        .catch((err) => {
+            usernameNotFound();
+        })
 }
 
 const showGithubDetails = (json) => {
+    const notFoundPar = document.getElementById('username_not_found')
+    notFoundPar.style.display = "none"
+
     const {avatar_url, name, created_at, login, bio, public_repos, followers, following, location, blog, twitter_username, company} = json;
     const date = new Date(created_at);
     const dateFormatter = new Intl.DateTimeFormat(
@@ -30,4 +35,22 @@ const showGithubDetails = (json) => {
     document.getElementById('website').innerText = blog ? blog : "Not available"
     document.getElementById('twitter_username').innerText = twitter_username ? twitter_username: "Not available"
     document.getElementById('organization').innerText = company ? company : "Not available"
+}
+
+const usernameNotFound = () => {
+    const notFoundPar = document.getElementById('username_not_found')
+    notFoundPar.style.display = "block"
+
+    document.getElementById('profile_img').setAttribute('src', "images/person-circle-white.svg")
+    document.getElementById('name').innerText = "Name"
+    document.getElementById('joined_date').innerText = "Date Created"
+    document.getElementById('github_username').innerText = "@username"
+    document.getElementById('bio').innerText = "Username's Bio"
+    document.getElementById('repo_count').innerText = "#"
+    document.getElementById('followers_count').innerText = "#"
+    document.getElementById('following_count').innerText = "#"
+    document.getElementById('location').innerText = "Location"
+    document.getElementById('website').innerText = "website.com"
+    document.getElementById('twitter_username').innerText = "@twitter"
+    document.getElementById('organization').innerText =  "Organization"
 }
